3ec1596bc8c53ec620daf50cd17bafee84a43f1b
Souvik Naskar <shouvik0104@gmail.com>
2024-09-15 20:03:03 +0530

    fixup! postgres database

--- podcasts_backend/podcasts_backend/settings_prod.py
+++ podcasts_backend/podcasts_backend/settings_prod.py
@@ -124,6 +124,7 @@ SPECTACULAR_SETTINGS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
