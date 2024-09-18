eaabf7c8d018b57d4af84c737c639ab23cd729ee
Souvik Naskar <shouvik0104@gmail.com>
2024-09-19 00:12:49 +0530

    post awsebcli changes

--- podcasts_backend/podcasts_backend/settings.py
+++ podcasts_backend/podcasts_backend/settings.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
+
 # Environmental variables
 env_path = Path(__file__).resolve().parent.parent / '.env'
 load_dotenv(dotenv_path=env_path)
@@ -13,6 +14,12 @@ load_dotenv(dotenv_path=env_path)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 ENV = os.getenv('ENV')
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+STATIC_ROOT = Path(__file__).resolve().parent.parent / 'static'
+STATIC_URL = 'static/'
 
 if ENV == 'DEVELOPMENT':
     from .settings_dev import *
